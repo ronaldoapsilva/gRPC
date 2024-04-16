b4320462706b1e4648ddc6fec783b92b23e295de
Ronaldo Aparecido da Silva <ronaldoapsilva_@hotmail.com>
2024-04-15 23:17:07 -0300

    CreateCategoryRequest

--- internal/service/category.go
+++ internal/service/category.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"io"
+	"log"
 
 	"github.com/ronaldoapsilva/gRPC/internal/database"
 	"github.com/ronaldoapsilva/gRPC/internal/pb"
@@ -67,4 +69,31 @@ func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetReq
 	}
 
 	return categoryResponse, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		log.Println(category)	
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+		
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+		// log.Println(categories)		
+	}
 }
